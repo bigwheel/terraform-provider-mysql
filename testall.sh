53178df0a531371c11cc06126ba7a64ba3e2c07c
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-08-23 00:04:02 +0900

    Split test script

--- testall.sh
+++ testall.sh
@@ -2,27 +2,14 @@
 
 set -eux
 
-echo $DB
-if [ "$DB" = "mysql:8.0" ]; then
-    DB_EXTRA='mysqld --default-authentication-plugin=mysql_native_password'
-else
-    DB_EXTRA=''
-fi
-
-make test
-make vet
 export MYSQL_HOST=127.0.0.1
 export MYSQL_PORT=3306
-export MYSQL_USERNAME=root
 export MYSQL_ENDPOINT="${MYSQL_HOST}:${MYSQL_PORT}"
+export MYSQL_USERNAME=root
 export MYSQL_PASSWORD=''
-docker pull $DB
-container_id=$(docker run --rm -d -p ${MYSQL_HOST}:${MYSQL_PORT}:${MYSQL_PORT} -e MYSQL_ALLOW_EMPTY_PASSWORD=1 -e MYSQL_ROOT_PASSWORD='' $DB $DB_EXTRA)
-trap "docker kill $container_id" EXIT
+export CONTAINER_NAME=terraform-provider-mysql
 
-while ! docker container exec $container_id mysql -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USERNAME} -e 'SELECT 1' &> /dev/null; do
-    echo 'Waiting for MySQL...'
-    sleep 1;
-done;
-docker container exec $container_id mysql -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USERNAME} -e "INSTALL PLUGIN mysql_no_login SONAME 'mysql_no_login.so';"
+./setup-and-init-db.sh
+trap "docker kill $CONTAINER_NAME" EXIT
+
 make testacc
